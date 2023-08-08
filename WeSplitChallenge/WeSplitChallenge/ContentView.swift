b048acddf789057b90f271d87219be941bd40319
Denis Postolov <95474761+denispostolov@users.noreply.github.com>
2023-08-08 12:33:45 +0200

    Project 1: WeSplit Challenge
    Goal: extend the WeSplit app 
    --------------------------------
    
    Challenge 1: Add a header to the third section
    
    Challenge 2: Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people.
    
    Challenge 3: Change the tip percentage picker to show a new screen rather than using a segmented control, and give it a wider range of options – everything from 0% to 100%. 
    
    Challenge 4: Rather than having to type .currency(code: Locale.current.currencyCode ?? "USD") in two places, can you make a new property to store the currency formatter?

---
+++ WeSplitChallenge/WeSplitChallenge/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  WeSplitChallenge
+//
+//  Created by Denis Postolov on 08/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    // Challenge 4
+    let currencyFormatter =  FloatingPointFormatStyle<Double>.Currency.currency(code: Locale.current.currency?.identifier ?? "EUR")
+    
+    
+    // a computed tuple property in order to solve Challenge 2
+    var totalAmount: (totalPerPerson: Double, grandTotal: Double) {
+        
+        let peopleCount = Double(numberOfPeople + 2) // handling the offset introduced by the ForEach in number of people Picker
+        
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        
+        let grandTotal = checkAmount + tipValue
+        
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return (amountPerPerson, grandTotal)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    
+                    TextField("Amount", value: $checkAmount, format:  currencyFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                } header: {
+                    Text("Specify the check amount and the number of people that will split the check")
+                }
+                
+                Section {
+                    Picker("Tip selection", selection: $tipPercentage){
+                        // Challenge 3
+                        ForEach(0..<101, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                        
+                    }
+                    
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                // Challenge 2
+                Section {
+                    Text(totalAmount.grandTotal, format:  currencyFormatter)
+                } header: {
+                    Text("Total amount for the check")
+                }
+                
+                Section {
+                    Text(totalAmount.totalPerPerson, format:
+                    currencyFormatter)
+                } header: {
+                    // Challenge 1
+                    Text("Per person will be an amout of: ")
+                }
+                
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer() // Flexible Space
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplitChallenge/WeSplitChallenge/WeSplitChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitChallengeApp.swift
+//  WeSplitChallenge
+//
+//  Created by Denis Postolov on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
