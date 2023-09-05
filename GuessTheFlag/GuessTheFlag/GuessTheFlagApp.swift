4c1e3537a39788fde137f3ee8ee9baa7025528aa
Denis Postolov <95474761+denispostolov@users.noreply.github.com>
2023-09-05 12:27:29 +0200

    GuessTheFlag
    Project 2: Guess the Flag app
    
    A simple quiz game app.
    
    (This version also contains the challenges requested)

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Denis Postolov on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
